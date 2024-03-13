1a1d982bc35a67bb6a0e8e1cb4b9568c14c388cd
sujan <sujanrintu@gmail.com>
2024-03-13 19:05:36 +0530

    @Named Qualifier and Create our own Qualifier @MassageQualifier

--- app/src/main/java/com/example/daggerandhilt/NotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerandhilt/NotificationServiceModule.kt
@@ -4,15 +4,23 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.migration.DisableInstallInCheck
+import javax.inject.Named
 
 @Module
 @DisableInstallInCheck
 class NotificationServiceModule {
-
+    @MassageQualifier
     @Provides
     fun getNotificationService() : NotificationService
     {
         // write logic which type or Service we want to pass
         return MassageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService):NotificationService
+    {
+        return emailService
     }
 }
--- app/src/main/java/com/example/daggerandhilt/UserRegistrationService.kt
+++ app/src/main/java/com/example/daggerandhilt/UserRegistrationService.kt
@@ -2,10 +2,11 @@ package com.example.daggerandhilt
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Named
 
 class UserRegistrationService @Inject constructor(
     private val userRepository: UserRepository,
-    private val notificationService:NotificationService
+    @MassageQualifier private val notificationService:NotificationService
 ){
     fun registerUser(email: String, password: String) {
         userRepository.saveUser(email,password)
