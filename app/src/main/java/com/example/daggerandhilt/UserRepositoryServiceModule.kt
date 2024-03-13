c35a370b46d24462fd24bf6b76b9e4442d04bca5
sujan <sujanrintu@gmail.com>
2024-03-13 19:08:59 +0530

    change UserRegistrationServiceModule to UserRepositoryServiceModule

--- app/src/main/java/com/example/daggerandhilt/UserRepositoryServiceModule.kt
+++ app/src/main/java/com/example/daggerandhilt/UserRepositoryServiceModule.kt
@@ -2,13 +2,11 @@ package com.example.daggerandhilt
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.migration.DisableInstallInCheck
 
 @Module
 @DisableInstallInCheck
-abstract class UserRegistrationServiceModule {
+abstract class UserRepositoryServiceModule {
 
     @Binds
     abstract  fun getUserRepositoryService(sqlRepository: SQLRepository):UserRepository
